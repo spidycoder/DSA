766286fc19b6fe6620261e4de123d5b13f6f2beb
Aditya Singh <98705653+spidycoder@users.noreply.github.com>
2022-10-07 19:50:18 +0530

    Add files via upload

---
+++ Linked-List/delete_node.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+using namespace std;
+
+Node *takeinput()
+{
+    int data;
+    cin >> data;
+    Node *head = NULL, *tail = NULL;
+    while (data != -1)
+    {
+        Node *newNode = new Node(data);
+        if (head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            tail = newNode;
+        }
+        cin >> data;
+    }
+    return head;
+}
+
+Node *deleteNode(Node *head, int pos)
+{
+    Node *temp = head;
+    if (head == NULL)
+    {
+        return NULL;
+    }
+    if (pos == 0)
+    {
+        head = temp->next;
+        return head;
+    }
+    int count = 0;
+    while (temp != NULL && count < pos - 1)
+    {
+        temp = temp->next;
+        count++;
+    }
+    if (temp != NULL && temp->next != NULL)
+    {
+
+        Node *current = temp->next;
+        Node *PrevPtr = current->next;
+        temp->next = PrevPtr;
+        delete current;
+    }
+    return head;
+}
+
+void print(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        Node *head = takeinput();
+        int pos;
+        cin >> pos;
+        head = deleteNode(head, pos);
+        print(head);
+    }
+
+    return 0;
+}
