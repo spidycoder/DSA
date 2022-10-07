f390c78e6b5a64713905a117311e84ff91db5455
Aditya Singh <98705653+spidycoder@users.noreply.github.com>
2022-10-07 14:37:51 +0530

    Add files via upload

---
+++ Linked-List/lenght_of_LL.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+
+
+
+Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newNode = new Node(data);
+		if (head == NULL)
+		{
+			head = newNode;
+			tail = newNode;
+		}
+		else
+		{
+			tail->next = newNode;
+			tail = newNode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+
+//Iterative Method to Find the lengt of LL
+int length(Node *head)
+{
+    //Write your code here
+    int count=0;
+    Node* temp=head;
+    while(temp!=NULL){
+         count++;
+        temp=temp->next;
+       
+    }
+    return count;
+}
+
+//Recursive method 
+int length(Node *head){
+    if(head==NULL){
+        return 0;
+    }
+    return 1+length(head->next);
+}
+
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		Node *head = takeinput();
+		cout << length(head) << endl;
+	}
+	return 0;
+}
