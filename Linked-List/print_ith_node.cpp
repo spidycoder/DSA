af1cae0fc255736a4b629fa7b3677a9acaa56f1c
Aditya Singh <98705653+spidycoder@users.noreply.github.com>
2022-10-07 19:49:47 +0530

    Add files via upload

---
+++ Linked-List/print_ith_node.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+using namespace std;
+
+Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newNode = new Node(data);
+		if (head == NULL)
+		{
+			head = newNode;
+			tail = newNode;
+		}
+		else
+		{
+			tail->next = newNode;
+			tail = newNode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+
+// function to print the ith node-data
+void printIthNode(Node *head, int i)
+{
+	Node *temp = head;
+	int count = 0;
+	while (temp != NULL && count < i)
+	{
+		temp = temp->next;
+		count++;
+	}
+	if (temp != NULL)
+	{
+		cout << temp->data;
+	}
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		Node *head = takeinput();
+		int pos;
+		cin >> pos;
+		printIthNode(head, pos);
+		cout << endl;
+	}
+	return 0;
+}
